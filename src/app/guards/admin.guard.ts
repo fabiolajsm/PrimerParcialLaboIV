d04fd9039965af7a8af65d8a06eaf649f45e4483
fabiolajsm <suarezfabiola17@gmail.com>
2024-05-29 20:58:05 -0300

    admin guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -4,14 +4,8 @@ import { AuthService } from '../services/auth.service';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const firebaseAuth = inject(AuthService);
-  const router = inject(Router);
-
   try {
     const isAdmin = firebaseAuth.getIsAdmin();
-    if (!isAdmin) {
-      router.navigateByUrl('');
-    }
-
     return isAdmin;
   } catch (error) {
     console.error('Error al verificar si es administrador:', error);
