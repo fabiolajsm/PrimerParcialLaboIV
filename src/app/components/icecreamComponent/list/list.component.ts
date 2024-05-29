36bae69b7bc70f91247b49cf0e845e5fb22466e2
fabiolajsm <suarezfabiola17@gmail.com>
2024-05-28 23:10:21 -0300

    listado

--- src/app/components/icecreamComponent/list/list.component.ts
+++ src/app/components/icecreamComponent/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { IceCream } from '../../../interfaces/icecream.interface';
 import { CommonModule } from '@angular/common';
+import { IceCreamService } from '../../../services/icecream.service';
 
 @Component({
   selector: 'app-list',
@@ -11,4 +12,18 @@ import { CommonModule } from '@angular/common';
 })
 export class ListComponent {
   @Output() selectedItem = new EventEmitter<IceCream>();
+  dataSource: any;
+  items!: IceCream[];
+
+  constructor(private icecreamService: IceCreamService) {}
+
+  ngOnInit(): void {
+    this.icecreamService.getData().subscribe((result) => {
+      this.items = result;
+    });
+  }
+
+  handleClick(item: IceCream) {
+    this.selectedItem.emit(item);
+  }
 }
